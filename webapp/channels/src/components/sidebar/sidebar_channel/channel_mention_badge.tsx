41b0db462481655fe078149c19fe16e56fd7b309
Sergey Rudachenko <rs@roistat.com>
2023-10-16 07:00:26 +0200

    badge fix

--- webapp/channels/src/components/sidebar/sidebar_channel/channel_mention_badge.tsx
+++ webapp/channels/src/components/sidebar/sidebar_channel/channel_mention_badge.tsx
@@ -7,9 +7,10 @@ import React from 'react';
 type Props = {
     unreadMentions: number;
     hasUrgent?: boolean;
+    isUnread?: boolean;
 };
 
-export default function ChannelMentionBadge({unreadMentions, hasUrgent}: Props) {
+export default function ChannelMentionBadge({unreadMentions, hasUrgent, isUnread}: Props) {
     if (unreadMentions > 0) {
         return (
             <span
@@ -21,5 +22,19 @@ export default function ChannelMentionBadge({unreadMentions, hasUrgent}: Props) 
         );
     }
 
+    if (isUnread) {
+        return (<span id='unreadMentions' className='badge' style={style.unread} />);
+    }
+
     return null;
 }
+
+const style = {
+    unread: {
+        borderRadius: '20px',
+        padding: '2px',
+        minWidth: '12px',
+        minHeight: '12px',
+        marginTop: '4px',
+    },
+};
